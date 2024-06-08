0eafac48ae841efa9ce6fa475753a00a726f4629
Zakari Adamu <ojonimizakari@gmail.com>
2024-06-08 10:55:54 +0100

    Add: add ConfirmationDialog file to practice a dialog pop-up in react

--- src/components/ConfirmationDialog.jsx
+++ src/components/ConfirmationDialog.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Dialog(props) {
+function Dialog({ children }) {
 	return (
 		<div className="modal">
 			<hr />
 			<p className="sandbox-obj">
 				--sandbox for: working with special children props
 			</p>
-			{props.children}
+			{children}
 		</div>
 	);
 }
@@ -18,6 +18,7 @@ function ConfirmationDialog() {
 			<h1 className="Dialog-title">Thanks!</h1>
 			<p className="Dialog-message">
 				We'll process your order in less than 24 hours.
+				
 			</p>
 		</Dialog>
 	);
