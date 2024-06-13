1a6b951a8f04ab9b93b5d8c96bb002e364c963ae
Zakari Adamu <ojonimizakari@gmail.com>
2024-06-13 10:15:25 +0100

    Add: create HOC component to illustrate code reusability and passing the same logic across mustiple components

--- src/components/HOC.jsx
+++ src/components/HOC.jsx
@@ -24,33 +24,39 @@ const withMousePosition = (WrappedComponent) => {
 		return <WrappedComponent {...props} mousePosition={mousePosition} />;
 	};
 };
-
+// first receiving component
 const PanelMouseLogger = ({ mousePosition }) => {
 	if (!mousePosition) {
 		return null;
 	}
 	return (
 		<div className="BasicTracker">
-			<p>Mouse position:</p>
-			<div className="Row">
+			<hr />
+			<p className="sandbox-obj">
+				--sandbox for: using HOC to provide data/logic to other receiving
+				components
+			</p>
+			<p className="d-flex">Mouse position:</p>
+			<div className="RowMousePosition">
 				<span>x: {mousePosition.x}</span>
 				<span>y: {mousePosition.y}</span>
 			</div>
 		</div>
 	);
 };
-
+// second receiving component
 const PointMouseLogger = ({ mousePosition }) => {
 	if (!mousePosition) {
 		return null;
 	}
 	return (
 		<p>
-			{mousePosition.x} {mousePosition.y}
+			({mousePosition.x}, {mousePosition.y})
 		</p>
 	);
 };
 
+// components alias so as to provide them with the HOC data/logic
 const PanelMouseTracker = withMousePosition(PanelMouseLogger);
 const PointMouseTracker = withMousePosition(PointMouseLogger);
 
